bb7e8f0c1b66135c4a4cf68b8bb7a43e5342dd4d
JoaoLucas <joaolucasdev28@gmail.com>
2026-05-12 10:25:35 -0300

    Service layer, component registration

--- src/main/java/com/projeto/spring_boot/controller/UserController.java
+++ src/main/java/com/projeto/spring_boot/controller/UserController.java
@@ -1,19 +1,34 @@
 package com.projeto.spring_boot.controller;
 
 import com.projeto.spring_boot.entities.User;
+import com.projeto.spring_boot.repositories.UserRepository;
+import com.projeto.spring_boot.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAllUsers() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "An4567");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAllUsers() {
+       List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
 
