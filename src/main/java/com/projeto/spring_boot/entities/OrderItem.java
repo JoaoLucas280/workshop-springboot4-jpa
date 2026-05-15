45f452800562ee421877f583c53d75855dc776bc
JoaoLucas <joaolucasdev28@gmail.com>
2026-05-15 09:56:36 -0300

    OrderItemPK and OrderItem entity created

---
+++ src/main/java/com/projeto/spring_boot/entities/OrderItem.java
@@ -0,0 +1,65 @@
+package com.projeto.spring_boot.entities;
+
+import com.projeto.spring_boot.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+@NoArgsConstructor
+public class OrderItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    @Getter
+    @Setter
+    private Integer quantity;
+    @Getter
+    @Setter
+    private Double price;
+
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price){
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/projeto/spring_boot/entities/pk/OrderItemPK.java
@@ -0,0 +1,38 @@
+package com.projeto.spring_boot.entities.pk;
+
+import com.projeto.spring_boot.entities.Order;
+import com.projeto.spring_boot.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Embeddable
+@Data
+public class OrderItemPK implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPK that = (OrderItemPK) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
